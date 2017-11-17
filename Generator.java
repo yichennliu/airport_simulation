700bcb8a6ca6d23b04e4010da6ab92ab6b7f6b6f
Yichen <liuyi@cip.ifi.lmu.de>
2017-11-17 14:45:32 +0100

    Generator erstellt

--- Generator.java
+++ Generator.java
@@ -1,4 +1,20 @@
 
 public class Generator {
-
+	private ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+	private double chance;
+	
+	public Generator(ArrayList<Waypoint> waypoints, double chance) {
+		this.chance = chance;
+		this.waypoints = waypoints;
+		
+	}
+	
+	public Plane execute() { 
+		if( chance<=Math.random()) {
+			return new Plane(this.waypoints, Plane.gettime());
+		} else{
+			return null; 
+		}
+	}
+	
 }
