a361004b2ce2b4c7f47a5dafc335906932563c7e
Andreas <Andreas@10.176.69.200>
2017-11-17 14:25:04 +0100

    Erstellung: Konstruktor, Getter und Setter

--- Node.java
+++ Node.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node {
 
+	private static double x;
+	private static double y;
+	private static String name;
+	private Enum kind;
+	List<Node>toNode = new ArrayList<Node>();
+	Node lastNode=null;
+	List<Integer>reserved = new ArrayList<Integer>();
+	
+	public Node() {	
+	}
+	
+	public Node(double x, double y, Enum kind, String name) {	
+		this.x= x;
+		this.y=y;
+		this.kind=kind;
+		
+	}
+	
+	public Node getLastNode() {
+		return lastNode;
+	}
+	
+	public void setLastNode(Node lastNode) {
+		this.lastNode = lastNode;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
--- Plane.java
+++ Plane.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Plane {
 
+	private List<Waypoint>waypoints = new ArrayList<Waypoint>();
+	private int inittime;
+	
+	public Plane(List<Waypoint> waypoints, int inittime) {
+		this.waypoints = waypoints;
+		this.inittime = inittime;
+	}
+	
 }
