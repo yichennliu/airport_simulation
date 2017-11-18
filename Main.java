0f4ded0747f6476b932d41a1b8b15cbc372e0747
Immanuel Bach <bachi@cip.ifi.lmu.de>
2017-11-18 18:32:46 +0100

    Main.java gefüllt

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+	  
+    Model model = new Model(screenWidth, screenHeight -30);
+    View view = new View(model, primaryStage,screenWidth, screenHeight-30);
+    new Controller(model, view);
+	
+	
+  }
+
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+ 
+}
