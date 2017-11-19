309cdf7bf28fc5f0eab117d35a129d0a850d5bc4
Immanuel Bach <bachi@cip.ifi.lmu.de>
2017-11-19 15:26:47 +0100

    direkter Zugriff auf das Canvas möglich

--- src/application/FlughafenController.java
+++ src/application/FlughafenController.java
@@ -1,17 +1,19 @@
 package application;
 
 import application.model.*;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.input.ScrollEvent;
 
 public class FlughafenController {
 	private FlughafenView view;
 	private Flughafen model;
-
+	
 	public FlughafenController(Flughafen model, FlughafenView view) {
 		this.model = model;
 		this.view = view;
+		Canvas canvas = this.view.getCanvas();
 		this.view.drawCanvas();
-		this.view.getCanvas().addEventHandler(ScrollEvent.SCROLL, e->{
+		canvas.addEventHandler(ScrollEvent.SCROLL, e->{
 			if (e.getDeltaY()>0) this.view.setZoomFactor(this.view.getZoomFactor()+0.1);
 			else this.view.setZoomFactor(this.view.getZoomFactor()-0.1);
 			this.view.drawCanvas(); // das kann später raus, wenn der Controller ohnehin in jedem Tick die drawCanvas()-Methode aufruft
