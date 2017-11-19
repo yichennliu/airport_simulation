09c2debcc3a8c0c5ca6970538cc54c8d2b7c3716
Immanuel Bach <bachi@cip.ifi.lmu.de>
2017-11-19 12:59:49 +0100

    fängt schon mal ein scrollEvent und ändert den zoomFakto in der View

--- src/application/FlughafenController.java
+++ src/application/FlughafenController.java
@@ -1,6 +1,7 @@
 package application;
 
 import application.model.*;
+import javafx.scene.input.ScrollEvent;
 
 public class FlughafenController {
 	private FlughafenView view;
@@ -9,10 +10,12 @@ public class FlughafenController {
 	public FlughafenController(Flughafen model, FlughafenView view) {
 		this.model = model;
 		this.view = view;
-		
-		
-		this.view.getStage().setTitle("Flughafen");
-		this.view.getStage().show();
+		this.view.drawCanvas();
+		this.view.getCanvas().addEventHandler(ScrollEvent.SCROLL, e->{
+			if (e.getDeltaY()>0) this.view.setZoomFactor(this.view.getZoomFactor()+0.1);
+			else this.view.setZoomFactor(this.view.getZoomFactor()-0.1);
+			this.view.drawCanvas();
+		});
 	}
 	
 }
