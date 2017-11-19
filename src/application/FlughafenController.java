c6bbeb2c28a2cdd741c038f78118f969fde44628
Immanuel Bach <bachi@cip.ifi.lmu.de>
2017-11-19 15:27:20 +0100

    Zugriff auf das Canvas möglich

--- src/application/FlughafenController.java
+++ src/application/FlughafenController.java
@@ -14,8 +14,8 @@ public class FlughafenController {
 		Canvas canvas = this.view.getCanvas();
 		this.view.drawCanvas();
 		canvas.addEventHandler(ScrollEvent.SCROLL, e->{
-			if (e.getDeltaY()>0) this.view.setZoomFactor(this.view.getZoomFactor()+0.1);
-			else this.view.setZoomFactor(this.view.getZoomFactor()-0.1);
+			if (e.getDeltaY()>0) this.view.setZoomFactor(this.view.getZoomFactor()+3.0);
+			else this.view.setZoomFactor(this.view.getZoomFactor()+3.0);
 			this.view.drawCanvas(); // das kann später raus, wenn der Controller ohnehin in jedem Tick die drawCanvas()-Methode aufruft
 		});
 	}
