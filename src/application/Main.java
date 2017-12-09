faf4ca720444c72686ed8449419f9b5ee73fee44
Andreas <Andreas@141.84.12.103>
2017-12-09 16:10:31 +0100

    Exception für fehlerhafte JSON Imports

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,13 +2,33 @@ package application;
 
 import application.model.*;
 import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Flughafen model = JSONImport.createFlughafen("application/json/small.json");
+    Flughafen model;
+    
+    try {
+    	model = JSONImport.createFlughafen("application/json/small.json");
+    	
+    }
+    catch(Exception e) {
+    	Alert alertWindow = new Alert(Alert.AlertType.ERROR,
+                e.getMessage(),
+                ButtonType.OK);
+    	alertWindow.setHeaderText("Fehler, bitte überprüfen Sie ihre JSON-Dartei");
+    	Image img = new Image("/application/source/Images/ber.jpg");
+    	alertWindow.setGraphic(new ImageView(img));
+    	alertWindow.show();
+    	return;
+    }
+    
     FlughafenView view = new FlughafenView(model, primaryStage);
     new FlughafenController(model, view);
   }
